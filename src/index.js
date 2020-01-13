50be9800c5dd37540c4e006fbafe600b2aba4a43
Matheus Jatczak Bertazzo <matheus.j.bertazzo@gmail.com>
2020-01-13 00:56:44 -0300

    Implements edge cases for -f argument

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 var parseArgs = require('minimist');
 var fs = require('fs');
 var yaml = require('js-yaml');
+var chalk = require('chalk');
 
 module.exports.main = function (args) {
   const options = parseArgs(args.slice(2));
   
-  const yamlFileContent = fs.readFileSync(process.cwd() + '/' + options['f'], 'utf8');
-  
-  const configuration = yaml.safeLoad(yamlFileContent);      
+  if (options['f'] && typeof options['f'] === 'string') {
+    const yamlFileContent = fs.readFileSync(process.cwd() + '/' + options['f'], 'utf8');
+    const configuration = yaml.safeLoad(yamlFileContent);     
+    console.log(configuration) ;
+  } else {
+    console.log(chalk.red('Template file is missing, please use the -f argument.'));
+  }
 }
--- test/index.test.js
+++ test/index.test.js
@@ -3,53 +3,82 @@ const chai = require('chai');
 const spies = require('chai-spies');
 const expect = chai.expect;
 
-const minimistMock = require('./mock/minimistMock');
+var minimistMock = require('./mock/minimistMock');
 const yamlJsMock = require('./mock/yamlJsMock');
 const fsMock = require('./mock/fsMock');
+const chalkMock = require('chalk');
 
 var cli;
 const cliArguments = ['some/path', 'another/path', '-f', 'template.yaml'];
 var minimistSpy;
 var yamlJsSpy;
 var fsSpy;
+var chalkSpy;
 
 describe('CLI', function() {
   describe('main()', function() {
-    before(() => {      
-      setupSpies();
+    before(() => {   
+      chai.use(spies);  
+      process.cwd = chai.spy.returns('/testPath');       
+    });
 
+    beforeEach(() => {
       cli = proxyquire('../src/index', {
         'minimist': minimistMock,
         'js-yaml': yamlJsMock,
-        'fs':fsMock
+        'fs':fsMock,
+        'chalk': chalkMock
       });
     });
 
     it('should parse arguments using minimist when called', function() {            
+      minimistSpy = chai.spy(minimistMock);
+
       cli.main(cliArguments);
       
       expect(minimistSpy).to.have.been.called;
     });
 
-    it('should read the template file using fs given the -f argument was passed', function() {            
+    it('should read the template file using fs given the -f argument was passed', function() {       
+      fsSpy = chai.spy.on(fsMock, 'readFileSync');
+
       cli.main(cliArguments);      
     
       expect(fsSpy).to.have.been.called.with('/testPath/template.yaml', 'utf8');      
     });
 
-    it('should interpret the template file using js-yaml given the -f argument was passed', function() {      
+    it('should interpret the template file using js-yaml given the -f argument was passed', function() { 
+      yamlJsSpy = chai.spy.on(yamlJsMock, 'safeLoad');
+
       cli.main(cliArguments);      
           
-      expect(yamlJsSpy).to.have.been.called.with('Mock YAML Content');
+      expect(yamlJsSpy).to.have.been.called.with('Mock YAML Content');      
+    });
+
+    it('should print a red error message given the -f argument is missing', function() {      
+      minimistMock = chai.spy.returns({});
+      chalkSpy = chai.spy.on(chalkMock, "red");
+
+      cli = proxyquire('../src/index', {
+        'minimist': minimistMock,
+        'chalk': chalkMock
+      });
+
+      cli.main(cliArguments);            
+      expect(chalkSpy).to.have.been.called;     
+    });
+
+    it('should print a red error message given the -f argument is incomplete', function() {      
+      minimistMock = chai.spy.returns({f: true});
+      chalkSpy = chai.spy.on(chalkMock, "red");
+
+      cli = proxyquire('../src/index', {
+        'minimist': minimistMock,
+        'chalk': chalkMock
+      });
+
+      cli.main(cliArguments);            
+      expect(chalkSpy).to.have.been.called;     
     });
   });
 });
-
-function setupSpies() {
-  chai.use(spies);
-  
-  process.cwd = chai.spy.returns('/testPath');
-  minimistSpy = chai.spy(minimistMock);
-  yamlJsSpy = chai.spy.on(yamlJsMock, 'safeLoad');
-  fsSpy = chai.spy.on(fsMock, 'readFileSync');
-}
