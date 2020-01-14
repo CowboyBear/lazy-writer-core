36dc6b199bd6e2d5601d98df550a2ec3cd48c7d3
Matheus Jatczak Bertazzo <matheus.j.bertazzo@gmail.com>
2020-01-13 22:15:56 -0300

    initial implementation for component creation using ng generate

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ var parseArgs = require('minimist');
 var fs = require('fs');
 var yaml = require('js-yaml');
 var chalk = require('chalk');
+const { exec } = require('child_process');
 
 module.exports.main = function (args) {
   const options = parseArgs(args.slice(2));
@@ -9,7 +10,13 @@ module.exports.main = function (args) {
   if (options['f'] && typeof options['f'] === 'string') {
     const yamlFileContent = fs.readFileSync(process.cwd() + '/' + options['f'], 'utf8');
     const configuration = yaml.safeLoad(yamlFileContent);     
-    console.log(configuration) ;
+    console.log(configuration);
+
+    exec('ng generate component ' + configuration.entity, (error, stdout, stderr) => {
+      console.log('error: ', error);
+      console.log('stdout: ', stdout);
+      console.log('stderr: ', stderr);
+    });
   } else {
     console.log(chalk.red('Template file is missing, please use the -f argument.'));
   }
--- test/index.test.js
+++ test/index.test.js
@@ -3,64 +3,62 @@ const chai = require('chai');
 const spies = require('chai-spies');
 const expect = chai.expect;
 
-var minimistMock = require('./mock/minimistMock');
-const yamlJsMock = require('./mock/yamlJsMock');
-const fsMock = require('./mock/fsMock');
 const chalkMock = require('chalk');
+const jsYamlSpy = {};
+const fsSpy = {};
+const childProcessSpy = {};
+var minimistSpy = {};
 
 var cli;
 const cliArguments = ['some/path', 'another/path', '-f', 'template.yaml'];
-var minimistSpy;
-var yamlJsSpy;
-var fsSpy;
-var chalkSpy;
 
 describe('CLI', function() {
   describe('main()', function() {
-    before(() => {   
+    before(() => {         
       chai.use(spies);  
+      console.log = chai.spy;
       process.cwd = chai.spy.returns('/testPath');       
     });
 
     beforeEach(() => {
+      jsYamlSpy.safeLoad = chai.spy.returns({entity: 'Person'})  
+      childProcessSpy.exec = chai.spy.returns((input, callback) => {});
+      minimistSpy = chai.spy.returns({f: 'template.yaml'});
+      fsSpy.readFileSync = chai.spy.returns('Mock YAML Content');
+
       cli = proxyquire('../src/index', {
-        'minimist': minimistMock,
-        'js-yaml': yamlJsMock,
-        'fs':fsMock,
-        'chalk': chalkMock
+        'minimist': minimistSpy,
+        'js-yaml': jsYamlSpy,
+        'fs':fsSpy,
+        'chalk': chalkMock,
+        'child_process': childProcessSpy
       });
     });
 
     it('should parse arguments using minimist when called', function() {            
-      minimistSpy = chai.spy(minimistMock);
-
       cli.main(cliArguments);
       
       expect(minimistSpy).to.have.been.called;
     });
 
-    it('should read the template file using fs given the -f argument was passed', function() {       
-      fsSpy = chai.spy.on(fsMock, 'readFileSync');
-
+    it('should read the template file using fs given the -f argument was passed', function() {             
       cli.main(cliArguments);      
     
-      expect(fsSpy).to.have.been.called.with('/testPath/template.yaml', 'utf8');      
+      expect(fsSpy.readFileSync).to.have.been.called.with('/testPath/template.yaml', 'utf8');      
     });
 
-    it('should interpret the template file using js-yaml given the -f argument was passed', function() { 
-      yamlJsSpy = chai.spy.on(yamlJsMock, 'safeLoad');
-
+    it('should interpret the template file using js-yaml given the -f argument was passed', function() {       
       cli.main(cliArguments);      
           
-      expect(yamlJsSpy).to.have.been.called.with('Mock YAML Content');      
+      expect(jsYamlSpy.safeLoad).to.have.been.called.with('Mock YAML Content');      
     });
 
     it('should print a red error message given the -f argument is missing', function() {      
-      minimistMock = chai.spy.returns({});
-      chalkSpy = chai.spy.on(chalkMock, "red");
+      minimistSpy = chai.spy.returns({});
+      var chalkSpy = chai.spy.on(chalkMock, "red");
 
       cli = proxyquire('../src/index', {
-        'minimist': minimistMock,
+        'minimist': minimistSpy,
         'chalk': chalkMock
       });
 
@@ -69,16 +67,21 @@ describe('CLI', function() {
     });
 
     it('should print a red error message given the -f argument is incomplete', function() {      
-      minimistMock = chai.spy.returns({f: true});
-      chalkSpy = chai.spy.on(chalkMock, "red");
+      minimistSpy = chai.spy.returns({f: true});
+      var chalkSpy = chai.spy.on(chalkMock, "red");
 
       cli = proxyquire('../src/index', {
-        'minimist': minimistMock,
+        'minimist': minimistSpy,
         'chalk': chalkMock
       });
 
       cli.main(cliArguments);            
       expect(chalkSpy).to.have.been.called;     
+    });
+
+    it('should generate a new component given a valid template file', function() {      
+      cli.main(cliArguments);            
+      expect(childProcessSpy.exec).to.have.been.called.with('ng generate component Person');     
     });
   });
 });
