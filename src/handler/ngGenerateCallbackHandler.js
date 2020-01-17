f4a3e9db24fb10028d99ab42d8ab79e5aa66be3e
Matheus Jatczak Bertazzo <matheus.j.bertazzo@gmail.com>
2020-01-17 00:50:56 -0300

    Adds codeGenerationComponent

--- src/handler/ngGenerateCallbackHandler.js
+++ src/handler/ngGenerateCallbackHandler.js
@@ -1,8 +1,19 @@
-module.exports = (error, stdout, stderr) => {
+const codeGenerationComponent = require('../components/codeGenerationComponent');
+
+function NgGenerateCallBackHandler(configuration, CLIOptions){
+  this.configuration = configuration;
+  this.CLIOptions = CLIOptions;
+}
+
+NgGenerateCallBackHandler.prototype.handle = function handle(error, stdout, stderr){  
   if (error) {
     console.log('Error while creating entity: ', stderr);
     return;
   } 
   
   console.log('Angular component created!');  
+  
+  codeGenerationComponent.generateHTML(this.configuration);
 }
+
+module.exports = NgGenerateCallBackHandler;
--- test/handler/ngGenerateCallbackHandler.test.js
+++ test/handler/ngGenerateCallbackHandler.test.js
@@ -2,29 +2,52 @@ const chai = require('chai');
 const spies = require('chai-spies');
 const expect = chai.expect;
 const handler = require('../../src/handler/ngGenerateCallbackHandler');
+const proxyquire = require('proxyquire');
 
-describe('ng generate callback handler', () => {
-  describe('callback', () => {
+var codeGenerationComponentMock = {};
+var NgGenerateCallBackHandler;
+
+describe('ngGenerateCallbackHandler', () => {
+  describe('handle()', () => {
     before(() => {         
       chai.use(spies);            
     });
 
     beforeEach(() => {
       console.log = chai.spy.returns((str) => {});
+      codeGenerationComponentMock.generateHTML = chai.spy.returns((configuration) => {});   
+      
+      NgGenerateCallBackHandler = proxyquire('../../src/handler/ngGenerateCallbackHandler', {
+        '../components/codeGenerationComponent': codeGenerationComponentMock
+      });
+
     });
 
     it('should log that the angular components were successfully created given no errors occured', () => {
-      handler(null, null, null);
+      var callbackHandler = new NgGenerateCallBackHandler(null, null);
+
+      callbackHandler.handle(null, null, null);
+
       expect(console.log).to.have.been.called.with('Angular component created!');
     });
 
     it('should log stderr given an error occured', () => {
       const error = 'error';
       const stderr = 'God dammit this is an error';
+      var callbackHandler = new NgGenerateCallBackHandler(null, null);
 
-      handler(error, null, stderr);
+      callbackHandler.handle(error, null, stderr);      
 
       expect(console.log).to.have.been.called.with('Error while creating entity: ', stderr);
+    });
+
+    it('should delegate HTML code generation to codeGenerationComponent given no errors occured', () => {
+      var mockConfiguration = {thisIsA: 'Mock'};
+      var callbackHandler = new NgGenerateCallBackHandler(mockConfiguration, null);
+
+      callbackHandler.handle(null, null, null);
+
+      expect(codeGenerationComponentMock.generateHTML).to.have.been.called.with(mockConfiguration);
     });
   });
 });
