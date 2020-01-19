7e43a96df00790be5422cdbdf1bf8a78688120a0
Matheus Jatczak Bertazzo <matheus.j.bertazzo@gmail.com>
2020-01-19 03:44:12 -0300

    adds text input creation

--- src/components/codeGenerationComponent.js
+++ src/components/codeGenerationComponent.js
@@ -3,12 +3,36 @@ const dirnameWrapper = require('../wrapper/dirnameWrappper');
 
 module.exports.generateHTML = (configuration) => {
   var entityName = configuration['entity'].toLowerCase();
+  const componentHTMLFilePath = process.cwd() + '/src/app/' + entityName + '/' + entityName + '.component.html';    
 
-  const formStartTemplateFilePath = dirnameWrapper + '/../assets/templates/html/form-start.html';
-  const componentHTMLFilePath = process.cwd() + '/src/app/' + entityName + '/' + entityName + '.component.html';  
+  textFields = configuration['fields'].filter((field) => { return field.type.toLowerCase() === 'string' });
   
-  var formStartContent = fs.readFileSync(formStartTemplateFilePath);
-
-  fs.writeFileSync(componentHTMLFilePath, '');
-  fs.appendFileSync(componentHTMLFilePath, formStartContent);    
+  cleanFile(componentHTMLFilePath);  
+  initForm(componentHTMLFilePath);
+  createTextFields(textFields, componentHTMLFilePath);  
+  closeForm(componentHTMLFilePath);
 };
+
+function cleanFile(filePath) {
+  fs.writeFileSync(filePath, '');
+}
+
+function initForm(filePath){  
+  fs.appendFileSync(filePath, getTemplateContent('html', 'form-start.html'));    
+}
+
+function createTextFields(fields, filePath) {    
+  fields.forEach((field) => {
+    fs.appendFileSync(filePath, getTemplateContent('html', 'text-input.html'));
+  });
+}
+
+function closeForm(filePath) {
+  fs.appendFileSync(filePath, getTemplateContent('html', 'form-end.html'));
+}
+
+function getTemplateContent(type, fileName) {
+  const templateFilePath = dirnameWrapper + '/../assets/templates/' + type + '/' + fileName;
+  return fs.readFileSync(templateFilePath);
+}
+
--- test/components/codeGenerationComponent.test.js
+++ test/components/codeGenerationComponent.test.js
@@ -2,19 +2,32 @@ const chai = require('chai');
 const spies = require('chai-spies');
 const expect = chai.expect;
 const proxyquire = require('proxyquire');
+
 const fsSpy = {};
 const dirnameWrapperMock = '/mock/relative/path';
+
+var mockComponentHTMLFilePath;
+var mockConfiguration;
 var component;
 
 describe('codeGenerationComponent', () => {
   describe('generateHTML()', () => {
     before(() => {         
-      chai.use(spies);            
+      chai.use(spies); 
+      process.cwd =  chai.spy.returns('/mock/base/path');          
+      mockComponentHTMLFilePath = process.cwd() + '/src/app/person/person.component.html';
+
+      mockConfiguration = {
+        entity: "Person",
+        fields: [
+          { name: 'name', type: 'string' },
+          { name: 'email', type: 'string' },
+          { name: 'notString', type: 'potato'}
+        ]      
+      };            
     });
 
-    beforeEach(() => {
-      process.cwd =  chai.spy.returns('/mock/base/path');      
-      
+    beforeEach(() => {              
       fsSpy.readFileSync = chai.spy();
       fsSpy.appendFileSync = chai.spy();
       fsSpy.writeFileSync = chai.spy();
@@ -25,31 +38,41 @@ describe('codeGenerationComponent', () => {
       });
     });
 
-    it('should cleanup the content of the component html file', () => {
-      var mockComponentHMTLFilePath = '/src/app/person/person.component.html';
-      
-      var mockConfiguration = {
-        entity: "Person"
-      };  
-
+    it('should cleanup the content of the component html file', () => {      
       component.generateHTML(mockConfiguration);  
 
-      expect(fsSpy.writeFileSync).to.have.been.called.with(process.cwd() + mockComponentHMTLFilePath, '');
+      expect(fsSpy.writeFileSync).to.have.been.called.with(mockComponentHTMLFilePath, '');
 
     });
 
-    it('should read from the form-start HTML template and print it to the component html file ', () => {      
-      var mockComponentHMTLFilePath = '/src/app/person/person.component.html';
-      var mockTemplateFilePath = dirnameWrapperMock + '/../assets/templates/html/form-start.html';
-
-      var mockConfiguration = {
-        entity: "Person"
-      };              
+    it('should initiate the form (form-start.html template) on the component html file', () => {            
+      var mockTemplateFilePath = dirnameWrapperMock + '/../assets/templates/html/form-start.html';         
 
       component.generateHTML(mockConfiguration);      
 
       expect(fsSpy.readFileSync).to.have.been.called.with(mockTemplateFilePath);
-      expect(fsSpy.appendFileSync).to.have.been.called.with(process.cwd() + mockComponentHMTLFilePath);
+      expect(fsSpy.appendFileSync).to.have.been.called.with(mockComponentHTMLFilePath);
+    });
+
+    it('should create a text field (text-input.html template) for every string field', () => {      
+      var mockTemplateFilePath = dirnameWrapperMock + '/../assets/templates/html/text-input.html';
+      const mockHTMLTemplate = "<input type=\"text\"/>";
+      fsSpy.readFileSync = chai.spy.returns(mockHTMLTemplate);
+ 
+      component.generateHTML(mockConfiguration);  
+
+      expect(fsSpy.readFileSync).to.have.been.called.with(mockTemplateFilePath);
+      expect(fsSpy.appendFileSync).to.have.been.second.called.with(mockComponentHTMLFilePath, mockHTMLTemplate);
+      expect(fsSpy.appendFileSync).to.have.been.third.called.with(mockComponentHTMLFilePath, mockHTMLTemplate);
+    });
+
+    it('should close the form (form-end.html template) after all fields are created', () => {            
+      var mockTemplateFilePath = dirnameWrapperMock + '/../assets/templates/html/form-end.html';         
+
+      component.generateHTML(mockConfiguration);      
+
+      expect(fsSpy.readFileSync).to.have.been.called.with(mockTemplateFilePath);
+      expect(fsSpy.appendFileSync).to.have.been.called.with(mockComponentHTMLFilePath);
     });
   });
 });
